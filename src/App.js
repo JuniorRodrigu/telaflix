332ca34e0d3745c4bac3f0e63abde4ae9e10530d
juniorRodrigu <juvenalrodrigues1717@gmail.com>
2022-06-17 01:35:16 -0300

    fim do dia

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow.js";
 import './App.css';
+import FeaturedMoveie from "./components/FeaturedMoveie.js";
 
 
 
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData ] = useState(null);
+
+
   useEffect(()=>{
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      
+
+
     }
     loadAll()
   },[]);
   return (
     <div className="page">
+        {featuredData &&   <FeaturedMoveie item={featuredData}/>
+        
+        }
+      
     <section className="lists">
     {movieList.map((item, key) => (
         <MovieRow key={key} title={item.title} items={item.items}/>
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import "./MovieRow.css"
+import "./MovieRow.css";
 
-
-
-export default ({title, items}) => {
-    return (
-        <div className="movieRow">
-         <h1>{title}</h1>
-                    {items.results.length > 0 && items.results.map((item, key) => (
-                        <div key={key} className="movieRow--item">
-                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-                        </div>
-                    ))}
-                </div>
-            
-    )
-}
+export default ({ title, items }) => {
+  return (
+    <div>
+      
+        <h1>{title}</h1>
+      
+      <div className="movieRow--listarea">
+        <div className="movieRow--list">
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+                <div key={key} className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
