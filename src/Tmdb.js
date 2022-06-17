8e5753bf5f6f067463ea9700a37dc8062fe8bbcb
juniorRodrigu <juvenalrodrigues1717@gmail.com>
2022-06-17 17:10:22 -0300

    baneer

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -73,5 +73,8 @@ export default {
         }
 
         return info
-    }
+    },
+
+  
+
 }
--- src/components/FeaturedMoveie.js
+++ src/components/FeaturedMoveie.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './FeaturedMoveie.css';
 
-export default () =>{
+export default ({item}) =>{
     return(
-        <div>
-            filme em destaque
-        </div>
+        
+        <section className="featured"  style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+
+        }}>
+           
+            <div className="featured--vertical">
+           
+                <div className="featured--horizontal"></div>
+            </div>
+        </section>
+        
     )
 }
