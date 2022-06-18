46c04c2dde7d5d1d88829994d13fe5f810d74b62
juniorRodrigu <juvenalrodrigues1717@gmail.com>
2022-06-18 16:12:39 -0300

    css apresentacao

--- src/components/FeaturedMoveie.js
+++ src/components/FeaturedMoveie.js
@@ -2,6 +2,15 @@ import React from "react";
 import './FeaturedMoveie.css';
 
 export default ({item}) =>{
+
+    let dataAno = new Date(item.first_air_date);
+
+    let generos = [];
+    for (let i in item.genres){
+        generos.push(item.genres[i].name);
+    }
+
+
     return(
         
         <section className="featured"  style={{
@@ -13,7 +22,24 @@ export default ({item}) =>{
            
             <div className="featured--vertical">
            
-                <div className="featured--horizontal"></div>
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                    <div className="featured--pointe">{item.vote_average} Pontos</div>
+                    <div className="featured--year">{dataAno.getFullYear()}</div>
+                    <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's': ''} </div>
+                    </div>
+                    <div className="featured--description">{item.overview}</div>
+
+                    <div className="featured--buttons">
+<a href={`/whatch/${item.id}`} className="featured--btbranco" >▷ Assistir</a>
+<a href={`/whatch/${item.id}`}className="featured--btpreto" >+ Minha Lista</a>
+
+                    </div>
+
+
+                    <div className="featured--genres"><strong>Generos </strong>{generos.join(', ')}</div>
+                </div>
             </div>
         </section>
         
