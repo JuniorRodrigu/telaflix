86fe90eedf08dcef76f9c6bcd767b0feab63bac3
juniorRodrigu <juvenalrodrigues1717@gmail.com>
2022-06-21 02:10:25 -0300

    busca

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
-import React from "react";
-import "./header.css";
+import React, { useState } from "react";
 
+import "./header.css";
+function handleInputChange() {
+const handleInputChange = (e) => {
+  e.preventDefault();
+      console.log(e.target.velue);
+};
+}
     export default ({preto}) => {
+      
+     
   return (
     <header className={preto ? 'preto' : ''}>
       <div className="header--logo">
         <h1>Telaflix</h1>
+
       </div>
       <div className="header--user">
+   <input name="sercha" id="sercha" onChange={handleInputChange}/>
+
         <a href="https://github.com/JuniorRodrigu"><img src="https://icones.pro/wp-content/uploads/2021/06/icone-github-rouge.png" alt="usuario"></img></a>
       </div>
     </header>
